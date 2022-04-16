3e94c80015c161dccfc41491bb989f097ca9dea9
Tommy <skywriter@gmail.com>
2022-04-16 10:37:59 +0100

    (*): added gift 4

--- data/daydata.js
+++ data/daydata.js
@@ -2,6 +2,6 @@ export const DayData = [
   { date: '2022-04-13', href: '/gift-1.png' },
   { date: '2022-04-15', href: '/gift-2.png' },
   { date: '2022-04-16', href: '/gift-3.jpg' },
-  { date: '2022-04-17', href: '/gift-4.png' },
+  { date: '2022-04-17', href: '/gift-4.jpg' },
   { date: '2022-04-18', href: '/gift-5.png' }
 ];
